76330aa953a80d1c065bedc66c5da24aa1bb7bd4
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2025-04-28 22:03:10 +0300

    [feature] added demo interface for assistant

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,6 @@ module org.example.timetableassistant {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.example.timetableassistant to javafx.fxml;
+    opens org.example.timetableassistant.constroller to javafx.fxml;
     exports org.example.timetableassistant;
 }
