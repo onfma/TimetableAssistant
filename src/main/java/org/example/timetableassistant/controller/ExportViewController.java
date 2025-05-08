13b2c83d887c575f9a60de78799e2c5458517513
Arrobert2202 <robertaron15@yahoo.ro>
2025-05-08 12:52:26 +0300

    exporter implementation

--- src/main/java/org/example/timetableassistant/controller/ExportViewController.java
+++ src/main/java/org/example/timetableassistant/controller/ExportViewController.java
@@ -3,26 +3,32 @@ package org.example.timetableassistant.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import org.example.timetableassistant.service.Exporter;
 
 public class ExportViewController {
+    private final Exporter exporter = new Exporter();
 
     @FXML
     private void handleExportStudents() {
+        exporter.generateGroupTimetable();
         showExportAlert("Export Orar Studenți");
     }
 
     @FXML
     private void handleExportTeachers() {
+        exporter.generateTeacherTimetable();
         showExportAlert("Export Orar Profesori");
     }
 
     @FXML
     private void handleExportRooms() {
+        exporter.generateRoomTimetable();
         showExportAlert("Export Orar Săli");
     }
 
     @FXML
     private void handleExportDisciplines() {
+        //exporter.generateDisciplineTimeTable();
         showExportAlert("Export Orar Discipline");
     }
 
